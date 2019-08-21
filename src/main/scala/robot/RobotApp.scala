34fc2df853460e74b932245bd30847104a69775e
Andy Wang <climber2002@gmail.com>
2019-08-21 10:40:45 +1000

    Refactoring

--- src/main/scala/robot/RobotApp.scala
+++ src/main/scala/robot/RobotApp.scala
@@ -7,7 +7,11 @@ object RobotApp extends App {
   def mainLoop(programState: ProgramState): IO[Unit] = for {
     _ <- IOFunctions.putStrLn("Please type your command")
     str <- IOFunctions.getLine
-    command = Command.parseCommand(str).flatMap(cmd => Validator.validateCommand(programState, cmd))
+    command = for {
+      cmd <- Command.parseCommand(str)
+      validatedCmd <- Validator.validateCommand(programState, cmd)
+    } yield validatedCmd
+
     state <-  command match {
       case Left(errorMessage) => for {
         _ <- IOFunctions.putStrLn(errorMessage)
