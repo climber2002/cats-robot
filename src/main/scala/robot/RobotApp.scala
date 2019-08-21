279b19180dc9556d8e63a36d6158450a62418770
Andy Wang <climber2002@gmail.com>
2019-08-21 10:38:56 +1000

    Add validator logic to RobotApp

--- src/main/scala/robot/RobotApp.scala
+++ src/main/scala/robot/RobotApp.scala
@@ -7,7 +7,8 @@ object RobotApp extends App {
   def mainLoop(programState: ProgramState): IO[Unit] = for {
     _ <- IOFunctions.putStrLn("Please type your command")
     str <- IOFunctions.getLine
-    state <- Command.parseCommand(str) match {
+    command = Command.parseCommand(str).flatMap(cmd => Validator.validateCommand(programState, cmd))
+    state <-  command match {
       case Left(errorMessage) => for {
         _ <- IOFunctions.putStrLn(errorMessage)
       } yield(programState)
